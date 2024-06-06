f4bb279fd14ee0b695ceca82878031b397996191
NeteroDano <72193562+NeteroDano@users.noreply.github.com>
2024-06-06 22:42:06 +0300

    for DB 1

--- server/data/db.js
+++ server/data/db.js
@@ -8,11 +8,18 @@ const mysql = require('mysql2');
 //     database: 'diploma'
 // });
 
+// const connection = mysql.createConnection({
+//     host: 'eu-cluster-west-01.k8s.cleardb.net',
+//     user: 'b58fcd7b104721',
+//     password: '4d81e108',
+//     database: 'heroku_0079a0c4500b5ab'
+// });
+
 const connection = mysql.createConnection({
-    host: 'eu-cluster-west-01.k8s.cleardb.net',
-    user: 'b58fcd7b104721',
-    password: '4d81e108',
-    database: 'heroku_0079a0c4500b5ab'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 connection.connect((err) => {
